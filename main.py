eae0ebc9f83724f1ecaebbbfa2b6fa187b20af48
Condor2012 <condor20121@gmail.com>
2023-06-08 15:24:38 +0300

    API

--- main.py
+++ main.py
@@ -4,13 +4,17 @@ import os
 import json
 from base64 import b64decode
 
+with open('API.txt', 'r', encoding='utf-8-sig') as fp:
+    API_KEY = fp.read().rstrip()
+
 prompt = input('The prompt: ')
-openai.api_key = os.getenv('API_KEY')
+# openai.api_key = os.getenv('API_KEY')
+openai.api_key = API_KEY
 
 response = openai.Image.create(
     prompt=prompt,
     n=1,
-    size='256x256',
+    size='512x512',
     response_format='b64_json'
 )
 
